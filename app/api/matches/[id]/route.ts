789d9cde817622cd3e97da8e3e5ccfb768e55ae7
Ivan <ivanralphdeluvio@gmail.com>
2026-06-07 09:35:17 +0800

    fix import paths

--- app/api/matches/[id]/route.ts
+++ app/api/matches/[id]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../../lib/auth'
-import { prisma } from '../../lib/prisma'
-import { pusherServer } from '../../lib/pusher'
+import { authOptions } from '../../../lib/auth'
+import { prisma } from '../../../lib/prisma'
+import { pusherServer } from '../../../lib/pusher'
 
 export const dynamic = 'force-dynamic'
 
